605124cb28829df5b38b7a4b4de0d191445188f4
franqq <githae.francis@gmail.com>
2016-05-16 10:26:45 +0300

    Added the flag states class

---
+++ api/src/main/java/org/openmrs/module/hospitalcore/util/FlagStates.java
@@ -0,0 +1,10 @@
+package org.openmrs.module.hospitalcore.util;
+
+/**
+ * Created by qqnarf on 5/16/16.
+ */
+public class FlagStates {
+    public static final int NOT_PROCESSED = 0;
+    public static final int PARTIALLY_PROCESSED = 1;
+    public static final int FULLY_PROCESSED = 2;
+}
