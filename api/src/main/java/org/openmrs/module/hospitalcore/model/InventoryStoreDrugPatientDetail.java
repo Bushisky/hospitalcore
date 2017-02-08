8ff6a437e2313acbdf5ff416cfcdc08eda0d0e47
Dennys Henry <daugmaud@gmail.com>
2017-02-08 17:16:18 +0300

    Added Issue Count

--- api/src/main/java/org/openmrs/module/hospitalcore/model/InventoryStoreDrugPatientDetail.java
+++ api/src/main/java/org/openmrs/module/hospitalcore/model/InventoryStoreDrugPatientDetail.java
@@ -24,39 +24,45 @@ package org.openmrs.module.hospitalcore.model;
 import java.io.Serializable;
 
 public class InventoryStoreDrugPatientDetail implements  Serializable {
-		 private static final long serialVersionUID = 1L;
-		 private Integer id;
-		 private InventoryStoreDrugPatient storeDrugPatient;
-		 private Integer quantity;
-		 private InventoryStoreDrugTransactionDetail transactionDetail;
-		public Integer getId() {
+    private static final long serialVersionUID = 1L;
+    private Integer id;
+    private InventoryStoreDrugPatient storeDrugPatient;
+    private Integer quantity;
+    private Integer issueCount;
+    private InventoryStoreDrugTransactionDetail transactionDetail;
+
+    public Integer getId() {
 			return id;
 		}
-		public void setId(Integer id) {
+
+    public void setId(Integer id) {
 			this.id = id;
 		}
-		
-		public Integer getQuantity() {
+    public Integer getQuantity() {
 			return quantity;
 		}
-		public void setQuantity(Integer quantity) {
+
+    public void setQuantity(Integer quantity) {
 			this.quantity = quantity;
 		}
-		
-		public InventoryStoreDrugPatient getStoreDrugPatient() {
+    public InventoryStoreDrugPatient getStoreDrugPatient() {
 			return storeDrugPatient;
 		}
-		public void setStoreDrugPatient(InventoryStoreDrugPatient storeDrugPatient) {
-			this.storeDrugPatient = storeDrugPatient;
-		}
-		public InventoryStoreDrugTransactionDetail getTransactionDetail() {
-			return transactionDetail;
-		}
-		public void setTransactionDetail(
-				InventoryStoreDrugTransactionDetail transactionDetail) {
-			this.transactionDetail = transactionDetail;
-		}
-		
-		 
-		 
+
+    public void setStoreDrugPatient(InventoryStoreDrugPatient storeDrugPatient) {
+        this.storeDrugPatient = storeDrugPatient;
+    }
+
+    public Integer getIssueCount() { return issueCount; }
+    public void setIssueCount(Integer issueCount) {
+        this.issueCount = issueCount;
+    }
+
+    public InventoryStoreDrugTransactionDetail getTransactionDetail() {
+        return transactionDetail;
+    }
+    public void setTransactionDetail(
+            InventoryStoreDrugTransactionDetail transactionDetail) {
+        this.transactionDetail = transactionDetail;
+    }
 }
